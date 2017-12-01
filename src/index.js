bd53fd21cd087559e108e63f119b2a808af5ae55
Dan Kiss <flowfast47@gmail.com>
2017-12-01 14:04:00 -0800

    custom clock with live per second update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
-var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, greeting, clock);
+function liveTime() {
+  var greeting = React.createElement('h1', {}, 'Hello, World!');
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  var app = React.createElement('div', {}, greeting, clock);
 
-ReactDOM.render(
-  app, //this is what changed from var greeting
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    app, //this is what changed from var greeting
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval(liveTime, 1000);
