0d9a7103d9af14cccf8c236c69762ea7e3e459ba
Dan Kiss <flowfast47@gmail.com>
2017-12-01 13:46:30 -0800

    add clock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var greeting = React.createElement('h1', {}, 'Hello, World!');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
-  greeting,
+  app, //this is what changed from var greeting
   document.getElementById('react-app-root')
 );
