2437fc3aad8fa2abd665763c4cbb206d3df77367
Dan Kiss <flowfast47@gmail.com>
2017-12-02 17:49:16 -0800

    update page to run with jsx via babel

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-  console.log(1)
-function liveTime() {
-  var greeting = React.createElement('h1', {}, 'Hello, World!');
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-  var app = React.createElement('div', {}, greeting, clock);
-  console.log(2)
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Help Queue'
+  ),
+  React.createElement(
+    'h3',
+    null,
+    '3 a'
+  ),
+  React.createElement(
+    'h3',
+    null,
+    'Thato and Haley'
+  ),
+  React.createElement(
+    'p',
+    null,
+    React.createElement(
+      'em',
+      null,
+      'Firebase wont save record'
+    )
+  )
+), document.getElementById('react-app-root'));
 
-  ReactDOM.render(
-    app, //this is what changed from var greeting
-    document.getElementById('react-app-root')
-  );
-  console.log(2.1)
-}
-  console.log(3)
-setInterval(liveTime, 1000);
-  console.log(4)
+// example #2 below note what is here is simplified via jsx file
+// var heading = React.createElement('h1', {}, 'Help Queue');
+// var ticketLocation = React.createElement('h3', {}, '3a');
+// var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+// var ticketIssue = React.createElement('h3', {}, 'Firebase wont save record');
+// var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
+//
+// ReactDOM.render(
+//   app,
+//   document.getElementById('react-app-root')
+// )
+
+//original below
+// function liveTime() {
+//   var greeting = React.createElement('h1', {}, 'Help Queue');
+//   var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+//   var app = React.createElement('div', {}, greeting, clock);
+//
+//   ReactDOM.render(
+//     app, //this is what changed from var greeting
+//     document.getElementById('react-app-root')
+//   );
+// }
+//
+// setInterval(liveTime, 1000);
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3 a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase wont save record</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
