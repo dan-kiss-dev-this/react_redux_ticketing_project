d44b2f7b98ccb61d112fc1b19fd875efe04ce0ce
Dan Kiss <flowfast47@gmail.com>
2017-12-19 20:04:05 -0800

    add component

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -5,17 +5,20 @@ class NewTicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this); 
   }
 
-  handleDisplayNewTicketForm(event){
-    console.log("new ticket button was clicked!")
+  handleDisplayingNewTicketForm(event){
+    console.log("New ticket button was clicked!");
+    this.setState({formVisibleOnPage: true});
   }
 
-  render() {
-    return(
-      <button onClick={this.handleDisplayNewTicketForm}>Request Help</button>
+  render(){
+    return (
+      <button onClick={this.handleDisplayingNewTicketForm}>Request Help</button>
     );
   }
+
 
 }
 
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,14 +1,61 @@
 import React from "react";
 import Ticket from "./Ticket";
 
-function TicketList(props){
-  
-  return (
-    <Ticket
-      location="3ay"
-      names="Thato and Haley"
-      issue="Firebase won't save record"/>
-  );
+var masterTicketList = [
+  {
+    names: "Thato and Haley",
+    location: "3A",
+    issue: "Firebase won't save record. Halp."
+  },
+  {
+    names: "Sleater and Kinney",
+    location: "4B",
+    issue: "Fox image not displaying on page, can only see duck?"
+  },
+  {
+    names: "Imani & Jacob",
+    location: "9F",
+    issue: "Donkey picture not displaying on hover in Zoology app. :("
+  }
+]
+
+class TicketList extends React.Component {
+//
+// function TicketList(props){
+  // constructor(props) {
+  //   super(props);
+  // }
+  //extra below
+  // componentDidMount(){
+  //
+  // }
+  // componentDidUpdate(){
+  //
+  // }
+  render() {
+    return (
+      <div>
+        <hr/>
+        {masterTicketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+                  location={ticket.location}
+                  issue={ticket.issue}
+                  key={index}/>
+        )}
+      </div>
+    );
+  }
 }
 
 export default TicketList;
+
+// old below
+// function TicketList(props){
+//
+//   return (
+//     <Ticket
+//       location="3ay"
+//       names="Thato and Haley"
+//       issue="Firebase won't save record"/>
+//   );
+// }
